be9baefeeaf6a56010c231f437f752b8d2882099
sartzz007 <126145248+sartzz007@users.noreply.github.com>
2024-03-01 18:03:03 +0000

    #

---
+++ Harkirat/02_harkirat.js
@@ -0,0 +1,35 @@
+// function printthem(a,b){
+//     console.log(a);
+//     console.log(b);
+// }
+
+// var a=1;
+// var b=2;
+// printthem(a,b);
+
+// // var temp=1;
+// // a=b;
+// // b=temp;
+
+
+// //second method
+
+// a=a+b;//a=3,b=2
+
+// b=a-b;//a=3,b=1
+
+// a=a-b;//a=2,b=1
+
+// printthem(a,b);
+
+// reversing aarays
+
+var a=[1, 2, 3 , 4];
+var n=a.length;
+
+for(var i=0; i<n/2;i++){
+    var startindex=i;
+    var endindex=n-1-i;
+    var temp=a[startindex];
+    
+}
